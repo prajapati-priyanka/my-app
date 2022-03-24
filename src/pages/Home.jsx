6fc0bc535d74dd5e240b67e1457cfdbb1a1c7b3c
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-03-24 22:49:24 +0530

    feat: add banner section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,22 @@
 import {Nav} from "../components/Navbar/Nav.jsx";
+import {Categories} from "../components/categories/Categories.jsx"
+import "./Home.css";
 
 
 const Home = () =>{
     return (
         <div>
             <Nav />
+            <section className="banner-section">
+                <img src="/assets/hero-image.jpg" alt="" />
+                <div className="banner-text-overlay">
+                    <h2 className="text-center">"The joy of dressing is an art." ~ John Galliano</h2>
+                    <a href="/productListing/product.html">
+                        <button className="btn btn-primary lg-text">Shop Now</button>
+                    </a>
+                </div>
+            </section>
+            <Categories />
         </div>
     )
 }
