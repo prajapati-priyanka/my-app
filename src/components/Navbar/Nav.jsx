6ff279eacdfc22f0a0b34f6cc6c39fee2c00db39
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-03-24 21:45:17 +0530

    fix: replace icons to Icon components

--- src/components/Navbar/Nav.jsx
+++ src/components/Navbar/Nav.jsx
@@ -1,35 +1,37 @@
 import React from "react";
-import "Nav.css";
+import "./Nav.css";
+import { MdOutlineFavoriteBorder } from "react-icons/md";
+import { BsCart } from "react-icons/bs";
+import { BsSearch } from "react-icons/bs";
 
 
 const Nav = ()=>{
     return (
         <header className="header">
-        <section className="header-section">
+        
             <div className="logo-container">
                 <a href="../index.html">
                     <img src="/assets/logo.png" alt="logo" />
+                    <span className="logo-name lg-text">Pine Apparel</span>
                 </a>
-                <span className="logo-name lg-text">Pine Apparel</span>
             </div>
-        </section>
-
-        <div className="header-section">
+       
             <form className="search-container">
-                <button type="submit" className="btn-search">
-                    <i className="fa fa-search"></i>
-                </button>
                 <input type="search" placeholder="Search Product" className="input-search" />
+                <button type="submit" className="btn-search">
+                    <BsSearch />
+                </button>
             </form>
-            <section className="header-icons">
+       
+            <div className="header-links">
                 <ul>
                     <li>
-                        <a href="/authentication/login/login.html"><button className="btn btn-primary">Login</button></a>
+                        <a href="/authentication/login/login.html"><button className="btn btn-primary md-text">Login</button></a>
                     </li>
                     <li>
                         <a href="/wishlist/wishlist.html">
                             <div className="icon badge">
-                                <i className="fa fa-heart-o"></i>
+                                <MdOutlineFavoriteBorder />
                                 <span className="badge-count">8</span>
                             </div>
                         </a>
@@ -37,14 +39,14 @@ const Nav = ()=>{
                     <li>
                         <a href="/cart/cart.html">
                             <div className="icon badge">
-                                <i className="fa fa-shopping-cart"></i>
+                                <BsCart />
                                 <span className="badge-count">8</span>
                             </div>
                         </a>
                     </li>
                 </ul>
-            </section>
-        </div>
+            </div>
+        
 
     </header>
 
