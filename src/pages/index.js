ff7c4d1432d2d30619a4e4dc14e34ff0f4a13df8
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-03-26 02:15:23 +0530

    feat: add index.js file for all pages export

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+export {Home} from "./home/Home.jsx";
+export {Product} from "./product/Product.jsx";
+export {Wishlist} from "./wishlist/Wishlist.jsx";
+export {Cart} from "./cart/Cart.jsx";
+export {Login} from "./authentication/login/Login.jsx";
+export {SignUp} from "./authentication/signup/SignUp.jsx";
+export {SignOut} from "./authentication/signout/SignOut.jsx";
