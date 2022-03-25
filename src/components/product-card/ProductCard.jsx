1cbb4c9e6606299b9d75be04e31c0855da977df1
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-03-25 17:42:18 +0530

    feat: recieved card content as props

--- src/components/product-card/ProductCard.jsx
+++ src/components/product-card/ProductCard.jsx
@@ -2,216 +2,30 @@ import { MdOutlineFavoriteBorder } from "react-icons/md";
 import "./ProductCard.css";
 
 
-const Card = () =>{
+const Card = (props) =>{
+    
+    const {title, subtitle,image, price, btnText} = props
     return (
-        <div className="product-display-container">
+        
             <div className="card vertical-card card-shadow">
                 <figure className="card-header">
-                    <img src="../assets/products/prod-1.jpg" alt="" />
+                    <img src={image} alt="" />
                 </figure>
                 <button className="card-floating-icon">
                    <MdOutlineFavoriteBorder title="Add to Wishlist"/>
                     
                 </button>
                 <section className="card-body">
-                    <h5 className="card-title md-text">Sangria </h5>
-                    <p className="card-subtitle md-text">Full Sleeve Padded Jacket</p>
+                    <h5 className="card-title md-text">{title}</h5>
+                    <p className="card-subtitle md-text">{subtitle}</p>
                     <div className="card-price">
-                        <span className="price-after-discount md-text">₹1,227</span>
+                        <span className="price-after-discount md-text">{price}</span>
 
                     </div>
-                    <button className="btn btn-primary">ADD TO CART</button>
+                    <button className="btn btn-primary">{btnText}</button>
                 </section>
             </div>
-            <div className="card vertical-card card-shadow">
-                <figure className="card-header">
-                    <img src="../assets/products/prod-2.jpg" alt="" />
-                </figure>
-                <button className="card-floating-icon">
-                <MdOutlineFavoriteBorder title="Add to Wishlist"/>
-                </button>
-                <section className="card-body">
-                    <h5 className="card-title md-text">Sangria</h5>
-                    <p className="card-subtitle md-text">Pink Georgette Ruffled Saree</p>
-                    <div className="card-price">
-                        <span className="price-after-discount md-text">₹1349</span>
-
-                    </div>
-                    <button className="btn btn-primary">ADD TO CART</button>
-                </section>
-            </div>
-            <div className="card vertical-card card-shadow">
-                <figure className="card-header">
-                    <img src="../assets/products/prod-3.jpg" alt="" />
-                </figure>
-                <button className="card-floating-icon">
-                    <MdOutlineFavoriteBorder title="Add to Wishlist"/>
-                </button>
-                <section className="card-body">
-                    <h5 className="card-title md-text">Ahalyaa</h5>
-                    <p className="card-subtitle md-text">Women Burgundy & Golden Printed Kurta with Trousers</p>
-                    <div className="card-price">
-                        <span className="price-after-discount md-text">₹1500</span>
-
-                    </div>
-                    <button className="btn btn-primary">ADD TO CART</button>
-                </section>
-            </div>
-            <div className="card vertical-card card-shadow">
-                <figure className="card-header">
-                    <img src="../assets/products/prod-4.jpg" alt="" />
-                </figure>
-                <button className="card-floating-icon">
-                    <MdOutlineFavoriteBorder title="Add to Wishlist"/>
-                </button>
-                <section className="card-body">
-                    <h5 className="card-title md-text">Ira Soleil</h5>
-                    <p className="card-subtitle md-text">Pink & Gold-Coloured Ethnic Motifs Peplum Top</p>
-                    <div className="card-price">
-                        <span className="price-after-discount md-text">₹1199</span>
-
-                    </div>
-                    <button className="btn btn-primary">ADD TO CART</button>
-                </section>
-            </div>
-            <div className="card vertical-card card-shadow">
-                <figure className="card-header">
-                    <img src="../assets/products/prod-5.jpg" alt="" />
-                </figure>
-                <button className="card-floating-icon">
-                    <MdOutlineFavoriteBorder title="Add to Wishlist"/>
-                </button>
-                <section className="card-body">
-                    <h5 className="card-title md-text">Marie Claire</h5>
-                    <p className="card-subtitle md-text">Women Pink Printed Top</p>
-                    <div className="card-price">
-                        <span className="price-after-discount md-text">₹545</span>
-
-                    </div>
-                    <button className="btn btn-primary">ADD TO CART</button>
-                </section>
-            </div>
-            <div className="card vertical-card card-shadow">
-                <figure className="card-header">
-                    <img src="../assets/products/prod-6.jpg" alt="" />
-                </figure>
-                <button className="card-floating-icon">
-                    <MdOutlineFavoriteBorder title="Add to Wishlist"/>
-                </button>
-                <section className="card-body">
-                    <h5 className="card-title md-text">STREET 9</h5>
-                    <p className="card-subtitle md-text">Women Blue Flared Mid-Rise Slash Knee Stretchable Jeans</p>
-                    <div className="card-price">
-                        <span className="price-after-discount md-text">₹14,000</span>
-
-                    </div>
-                    <button className="btn btn-primary">ADD TO CART</button>
-                </section>
-            </div>
-            <div className="card vertical-card card-shadow">
-                <figure className="card-header">
-                    <img src="../assets/products/prod-7.jpg" alt="" />
-                </figure>
-                <button className="card-floating-icon">
-                    <MdOutlineFavoriteBorder title="Add to Wishlist"/>
-                </button>
-                <section className="card-body">
-                    <h5 className="card-title md-text">SASSAFRAS</h5>
-                    <p className="card-subtitle md-text">Women Fuchsia Relaxed Fit High-Rise Jeans</p>
-                    <div className="card-price">
-                        <span className="price-after-discount md-text">₹999</span>
-
-                    </div>
-                    <button className="btn btn-primary">ADD TO CART</button>
-                </section>
-            </div>
-            <div className="card vertical-card card-shadow">
-                <figure className="card-header">
-                    <img src="../assets/products/prod-8.jpg" alt="" />
-                </figure>
-                <button className="card-floating-icon">
-                    <MdOutlineFavoriteBorder title="Add to Wishlist"/>
-                </button>
-                <section className="card-body">
-                    <h5 className="card-title md-text">Sangria</h5>
-                    <p className="card-subtitle md-text">Women Purple Solid Georgette Saree with Embellished Belt   </p>
-                    <div className="card-price">
-                        <span className="price-after-discount md-text">₹1,500</span>
-
-                    </div>
-                    <button className="btn btn-primary">ADD TO CART</button>
-                </section>
-            </div>
-            <div className="card vertical-card card-shadow">
-                <figure className="card-header">
-                    <img src="../assets/products/prod-9.jpg" alt="" />
-                </figure>
-                <button className="card-floating-icon">
-                    <MdOutlineFavoriteBorder title="Add to Wishlist"/>
-                </button>
-                <section className="card-body">
-                    <h5 className="card-title md-text">Suta</h5>
-                    <p className="card-subtitle md-text">Pink & Golden Solid Zari Pure Linen Saree</p>
-                    <div className="card-price">
-                        <span className="price-after-discount md-text">₹3,500</span>
-
-                    </div>
-                    <button className="btn btn-primary">ADD TO CART</button>
-                </section>
-            </div>
-            <div className="card vertical-card card-shadow">
-                <figure className="card-header">
-                    <img src="../assets/products/prod-10.jpg" alt="" />
-                </figure>
-                <button className="card-floating-icon">
-                    <MdOutlineFavoriteBorder title="Add to Wishlist"/>
-                </button>
-                <section className="card-body">
-                    <h5 className="card-title md-text">INDYA</h5>
-                    <p className="card-subtitle md-text">ndya Navy Sequin Embroidered Panel Kurta</p>
-                    <div className="card-price">
-                        <span className="price-after-discount md-text">₹1,500</span>
-
-                    </div>
-                    <button className="btn btn-primary">ADD TO CART</button>
-                </section>
-            </div>
-            <div className="card vertical-card card-shadow">
-                <figure className="card-header">
-                    <img src="../assets/products/prod-11.jpg" alt="" />
-                </figure>
-                <button className="card-floating-icon">
-                    <MdOutlineFavoriteBorder title="Add to Wishlist"/>
-                </button>
-                <section className="card-body">
-                    <h5 className="card-title md-text">Anouk</h5>
-                    <p className="card-subtitle md-text">Navy Blue Printed Peplum Top</p>
-                    <div className="card-price">
-                        <span className="price-after-discount md-text">₹700</span>
-
-                    </div>
-                    <button className="btn btn-primary">ADD TO CART</button>
-                </section>
-            </div>
-            <div className="card vertical-card card-shadow">
-                <figure className="card-header">
-                    <img src="../assets/products/prod-12.jpg" alt="" />
-                </figure>
-                <button className="card-floating-icon">
-                    <MdOutlineFavoriteBorder title="Add to Wishlist"/>
-                </button>
-                <section className="card-body">
-                    <h5 className="card-title md-text">La Zoire</h5>
-                    <p className="card-subtitle md-text">Lime Green Mini A-Line Dress</p>
-                    <div className="card-price">
-                        <span className="price-after-discount md-text">₹20,999</span>
-
-                    </div>
-                    <button className="btn btn-primary">ADD TO CART</button>
-                </section>
-            </div>
-
-        </div>
+      
     )
 }
 
