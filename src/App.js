2b2fdc4e1bf2ba06f07114851254ad5d3cadb2e5
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-03-26 01:39:31 +0530

    feat: import all pages to App

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-// import {Home} from "./pages/home/Home.jsx";
-// import {Product} from "./pages/product/Product.jsx";
-// import { Wishlist } from "./pages/wishlist/Wishlist.jsx";
-// import { Cart } from "./pages/cart/Cart.jsx";
-// import { Login } from "./pages/authentication/login/Login.jsx";
-import { SignUp } from "./pages/authentication/signup/SignUp";
+import {Home} from "./pages/home/Home.jsx";
+import {Product} from "./pages/product/Product.jsx";
+import { Wishlist } from "./pages/wishlist/Wishlist.jsx";
+import { Cart } from "./pages/cart/Cart.jsx";
+import { Login } from "./pages/authentication/login/Login.jsx";
+import { SignUp } from "./pages/authentication/signup/SignUp.jsx";
+import { SignOut } from "./pages/authentication/signout/SignOut.jsx";
 
-// import { SignUp } from "./pages/authentication/signup/SignUp.jsx";
+
 function App() {
   return (
     <div className="App">
-     {/* <Home /> */}
-     {/* <Product /> */}
-     {/* <Wishlist /> */}
-     {/* <Cart /> */}
-     {/* <Login /> */}
+     <Home />
+     <Product />
+     <Wishlist />
+     <Cart />
+     <Login />
      <SignUp />
+     <SignOut />
     </div>
   );
 }
