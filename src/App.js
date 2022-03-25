52cc2d640b75c10314060cfc0983fe5ae2ef7f70
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-03-26 01:01:50 +0530

    feat: import login page to App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 // import {Home} from "./pages/home/Home.jsx";
 // import {Product} from "./pages/product/Product.jsx";
 // import { Wishlist } from "./pages/wishlist/Wishlist.jsx";
-import { Cart } from "./pages/cart/Cart.jsx";
+// import { Cart } from "./pages/cart/Cart.jsx";
+import { Login } from "./pages/authentication/login/Login.jsx";
 function App() {
   return (
     <div className="App">
      {/* <Home /> */}
      {/* <Product /> */}
      {/* <Wishlist /> */}
-     <Cart />
+     {/* <Cart /> */}
+     <Login />
     </div>
   );
 }
