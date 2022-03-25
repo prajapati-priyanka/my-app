77e154c84005d5b845b3b6084764f2a937909b48
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-03-25 19:35:46 +0530

    feat: import Cart page to App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 // import {Home} from "./pages/home/Home.jsx";
 // import {Product} from "./pages/product/Product.jsx";
-import { Wishlist } from "./pages/wishlist/Wishlist.jsx";
+// import { Wishlist } from "./pages/wishlist/Wishlist.jsx";
+import { Cart } from "./pages/cart/Cart.jsx";
 function App() {
   return (
     <div className="App">
      {/* <Home /> */}
      {/* <Product /> */}
-     <Wishlist />
+     {/* <Wishlist /> */}
+     <Cart />
     </div>
   );
 }
