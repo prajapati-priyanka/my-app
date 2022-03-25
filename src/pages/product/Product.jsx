87782c3de02ebfc1378c269b2792292dc522a2f2
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-03-25 14:00:36 +0530

    feat: add product page

--- src/pages/product/Product.jsx
+++ src/pages/product/Product.jsx
@@ -1,9 +1,18 @@
 import {Nav} from "../../components/navbar/Nav.jsx";
+import {Filter} from "../../components/filter/Filter.jsx";
+import { ProductList } from "../../components/productlist/ProductList.jsx";
+import {Footer} from "../../components/footer/Footer.jsx";
+import "./Product.css";
 
 const Product = () =>{
     return (
         <div>
             <Nav />
+            <div className="product-container">
+                <Filter />
+                <ProductList />
+            </div>
+            <Footer />
         </div>
     )
 }
