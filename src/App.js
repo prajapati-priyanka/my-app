cc61e3d27c403cc852febf8736e9442ab89fcebf
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-03-26 02:16:01 +0530

    refactor: import all pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,4 @@
-import {Home} from "./pages/home/Home.jsx";
-import {Product} from "./pages/product/Product.jsx";
-import { Wishlist } from "./pages/wishlist/Wishlist.jsx";
-import { Cart } from "./pages/cart/Cart.jsx";
-import { Login } from "./pages/authentication/login/Login.jsx";
-import { SignUp } from "./pages/authentication/signup/SignUp.jsx";
-import { SignOut } from "./pages/authentication/signout/SignOut.jsx";
+import {Home, Product, Wishlist, Cart, Login, SignUp, SignOut} from "./pages"
 
 
 function App() {
