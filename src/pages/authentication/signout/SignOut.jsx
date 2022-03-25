a741ad05c02a7e3591026f9a15b484b0c0a1e75a
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-03-26 01:29:31 +0530

    feat: add signout page

---
+++ src/pages/authentication/signout/SignOut.jsx
@@ -0,0 +1,24 @@
+import { Nav } from "../../../components/navbar/Nav";
+import {FaCheck } from "react-icons/fa";
+import "../login/Login.css";
+import "./SignOut.css"
+
+const SignOut = () => {
+    return (
+        <div>
+            <Nav />
+             <main className="form-wrapper">
+        <div className="form-login form-signout">
+          <span className="check-btn x-lg-text">
+                <FaCheck />
+          </span>
+           
+            <div className="signout-msg lg-text text-center">You have succesfully logged out !!</div>
+            <a href="/authentication/login/login.html" className="text-center md-text">Back to Login</a>
+        </div>
+    </main>
+        </div>
+    )
+}
+
+export {SignOut}
