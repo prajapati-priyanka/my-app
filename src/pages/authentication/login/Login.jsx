95ff9fb2ddda160b53f705d00334062b4a68db89
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-03-26 02:02:29 +0530

    fix: change attribute for to htmlFor

--- src/pages/authentication/login/Login.jsx
+++ src/pages/authentication/login/Login.jsx
@@ -1,34 +1,40 @@
 import { MdOutlineArrowForwardIos } from "react-icons/md";
-import {Nav} from "../../../components/navbar/Nav.jsx";
-import "./Login.css"
+import { Nav } from "../../../components/navbar/Nav.jsx";
+import "./Login.css";
 
-const Login = () =>{
-    return (
-        <div>
-            <Nav />
-             <main className="form-wrapper">
-                 <div className="form-login">
-                    <h3 className="form-title text-center x-lg-text">Login</h3>
-                    <form className="form-content md-text">
-                    <label for="email">Email Address</label>
-                    <input type="email" id="email" placeholder="tanaypratap@neog.camp" />
-                    <label for="pass">Password</label>
-                    <input type="password" id="pass" placeholder="*******" />
-                 <div className="user-control">
-                    <input type="checkbox" className="input-check" />
-                    <label>Remember Me ?</label>
-                    <a className="forgot-pass">Forgot your Password </a>
-                </div>
-                <button className="btn btn-primary md-text" >LOGIN</button>
-                <p className="new-account text-center"><a href="/authentication/signup/signup.html">Create New Account
-                <MdOutlineArrowForwardIos className="forward-icon" /></a></p>
-            </form>
+const Login = () => {
+  return (
+    <div>
+      <Nav />
+      <main className="form-wrapper">
+        <div className="form-login">
+          <h3 className="form-title text-center x-lg-text">Login</h3>
+          <form className="form-content md-text">
+            <label htmlFor="email">Email Address</label>
+            <input
+              type="email"
+              id="email"
+              placeholder="tanaypratap@neog.camp"
+            />
+            <label htmlFor="pass">Password</label>
+            <input type="password" id="pass" placeholder="*******" />
+            <div className="user-control">
+              <input type="checkbox" className="input-check" />
+              <label>Remember Me ?</label>
+              <a className="forgot-pass">Forgot your Password </a>
+            </div>
+            <button className="btn btn-primary md-text">LOGIN</button>
+            <p className="new-account text-center">
+              <a href="/authentication/signup/signup.html">
+                Create New Account
+                <MdOutlineArrowForwardIos className="forward-icon" />
+              </a>
+            </p>
+          </form>
         </div>
-    </main>
+      </main>
+    </div>
+  );
+};
 
-        </div>
-       
-    )
-}
-
-export {Login}
+export { Login };
