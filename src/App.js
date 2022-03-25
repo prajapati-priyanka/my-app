14ce849d07d5175ab20ebf86220a2b1d7d258d23
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-03-25 12:48:46 +0530

    feat: import product page to app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import {Home} from "./pages/home/Home.jsx"
-
+// import {Home} from "./pages/home/Home.jsx";
+import {Product} from "./pages/product/Product.jsx";
 function App() {
   return (
     <div className="App">
-     <Home />
+     {/* <Home /> */}
+     <Product />
     </div>
   );
 }
