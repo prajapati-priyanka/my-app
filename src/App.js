29be5f0d733554f1a8816d5ceeae8cbe98d1640c
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-03-26 15:00:35 +0530

    Revert "feat: wrap App around BrowserRouter"
    This reverts commit 798cabcf9bc8cc820fd652832cfc2c5cee27776e.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import { Home, Product, Wishlist, Cart, Login, SignUp, SignOut } from "./pages";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="product" element={<Product />} />
-        <Route path="wishlist" element={<Wishlist />} />
-        <Route path="cart" element={<Cart />} />
-        <Route path="login" element={<Login />} />
-        <Route path="signup" element={<SignUp />} />
-        <Route path="signout" element={<SignOut />} />
-      </Routes>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="product" element={<Product />} />
+          <Route path="wishlist" element={<Wishlist />} />
+          <Route path="cart" element={<Cart />} />
+          <Route path="login" element={<Login />} />
+          <Route path="signup" element={<SignUp />} />
+          <Route path="signout" element={<SignOut />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
