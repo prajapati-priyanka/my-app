bf31592b6bdb5d851a0dd2d2e25a11ed3b943a95
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-03-29 13:05:56 +0530

    feat: Provide product-context to App

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
 import { BrowserRouter } from "react-router-dom";
+import { ProductProvider } from "./context/product-context";
 
 // Call make Server
 makeServer();
@@ -11,7 +12,9 @@ makeServer();
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ProductProvider>
+        <App />
+      </ProductProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
