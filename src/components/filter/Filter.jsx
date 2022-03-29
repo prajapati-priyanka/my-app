c8cbb0ea2ccb1ece09d2a415644148b20820cd97
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-03-29 13:20:45 +0530

    feat: added functionality to all inputs
    Added onChange event handler to all the inputs.
    In price range slider, values are added for better user experience

--- src/components/filter/Filter.jsx
+++ src/components/filter/Filter.jsx
@@ -1,6 +1,8 @@
+import { useProduct } from "../../context/product-context";
 import "./Filter.css";
-
 const Filter = () => {
+const{state, dispatch} = useProduct()
+console.log("In Filter",state, dispatch)
     return (
         <aside className="product-filter-container">
             <div className="product-filter-clear">
@@ -10,65 +12,93 @@ const Filter = () => {
             <div className="filter-type">
                 <h3 className="filter-type-heading lg-text">Price</h3>
                 <div className="filter-type-container price-filter">
-                    <label for="myRange" className="md-text">₹500-₹5000</label>
-                    <input type="range" min="200" max="10,000" value="1000" className="slider" id="myRange" />
+                    <label htmlFor="myRange" className="md-text">price slider</label>
+                    <input type="range" min="600" max="3000" value={state.price} className="slider" id="myRange"  list="marks"
+                 step="400" onChange={(e)=>dispatch({type:"PRICE_SLIDER", payload: e.target.value})}/>
+                 <dataList id="marks"
+                  className="datalist-options"
+                  style={{
+                        display: "flex",
+                        justifyContent: "space-between",
+                         fontSize: "1rem"
+                        }}
+                    >
+                    <option value="600" label="600"></option>
+                    <option value="1000" label="1000"></option>
+                    <option value="1400" label="1400"></option>
+                    <option value="1800" label="1800"></option>
+                    <option value="2200" label="2200"></option>
+                    <option value="2600" label="2600"></option>
+                </dataList>
                 </div>
             </div>
 
             <div className="filter-type">
                 <h3 className="filter-type-heading lg-text">Category</h3>
                 <div className="filter-type-container md-text">
-                    <input type="checkbox" id="top" className="input-check" />
-                    <label for="top">Top</label>
+                    <input type="checkbox" id="top" className="input-check" name="category-sort"  checked={state.sortByCategory ==="SORT_BY_TOP_CATEGORY"} onChange={()=>dispatch({type:"SORT_BY_TOP_CATEGORY"})} />
+                    <label htmlFor="top">Top</label>
                 </div>
                 <div className="filter-type-container md-text">
-                    <input type="checkbox" id="jeans" className="input-check" />
-                    <label for="jeans">Jeans</label>
+                    <input type="checkbox" id="jeans" className="input-check" name="category-sort"  checked={state.sortByCategory ==="SORT_BY_JEANS_CATEGORY"} onChange={()=>dispatch({type:"SORT_BY_JEANS_CATEGORY"})}/>
+                    <label htmlFor="jeans">Jeans</label>
                 </div>
                 <div className="filter-type-container md-text">
-                    <input type="checkbox" id="dresses" className="input-check" />
-                    <label for="dresses">Dresses</label>
+                    <input type="checkbox" id="dresses" className="input-check" name="category-sort" checked={state.sortByCategory=== "SORT_BY_DRESSES_CATEGORY"} onChange={()=>dispatch({type:"SORT_BY_DRESSES_CATEGORY"})} />
+                    <label htmlFor="dresses">Dresses</label>
                 </div>
                 <div className="filter-type-container md-text">
-                    <input type="checkbox" id="kurta-set" className="input-check" />
-                    <label for="kurta-set">Kurta Set</label>
+                    <input type="checkbox" id="kurta-set" className="input-check" name="category-sort"  checked={state.sortByCategory === "SORT_BY_KURTA_SET_CATEGORY"} onChange={()=>dispatch({type:"SORT_BY_KURTA_SET_CATEGORY"})}/>
+                    <label htmlFor="kurta-set">Kurta Set</label>
                 </div>
                 <div className="filter-type-container md-text">
-                    <input type="checkbox" id="saree" className="input-check" />
-                    <label for="saree">Sarees</label>
+                    <input type="checkbox" id="saree" className="input-check" name="category-sort" checked={state.sortByCategory=== "SORT_BY_SAREES_CATEGORY"} onChange={()=>dispatch({type:"SORT_BY_SAREES_CATEGORY"})} />
+                    <label htmlFor="saree">Sarees</label>
+                </div>
+            </div>
+            <div className="filter-type">
+                <h3 className="filter-type-heading lg-text">Availability</h3>
+                <div className="filter-type-container md-text">
+                    <input type="checkbox" id="out-of-stock" className="input-check" checked={state.showAll} onChange={()=>dispatch({type:"FILTER_OUT_OF_STOCK"})}/>
+                    <label htmlFor="out-of-stock">Include Out Of Stock</label>
                 </div>
             </div>
             <div className="filter-type">
                 <h3 className="filter-type-heading lg-text">Rating</h3>
                 <div className="filter-type-container md-text">
-                    <input type="radio" id="four-rating" className="input-radio" />
-                    <label for="four-rating">4 stars & above</label>
+                    <input type="radio" id="four-rating" className="input-radio" name="rating-sort" value="4 stars & above" checked={state.sortByCategory === "4 stars & above "} onChange={(e)=>dispatch({type: "FILTER_RATING" , payload: e.target.value})}/>
+                    <label htmlFor="four-rating">4 stars & above</label>
                 </div>
                 <div className="filter-type-container md-text">
-                    <input type="radio" id="three-rating" className="input-radio" />
-                    <label for="three-rating">3 stars & above</label>
+                    <input type="radio" id="three-rating" className="input-radio" name="rating-sort" value="3 stars & above" checked={state.sortByCategory === "3 stars & above"} onChange={(e)=>dispatch({type: "FILTER_RATING" , payload: e.target.value})}/>
+                    <label htmlFor="three-rating">3 stars & above</label>
                 </div>
                 <div className="filter-type-container md-text">
-                    <input type="radio" id="two-rating" className="input-radio" />
-                    <label for="two-rating">2 stars & above</label>
+                    <input type="radio" id="two-rating" className="input-radio" name="rating-sort" value="2 stars & above" checked={state.sortByCategory === "2 stars & above"} onChange={(e)=>dispatch({type: "FILTER_RATING" , payload: e.target.value})}/>
+                    <label htmlFor="two-rating">2 stars & above</label>
                 </div>
 
                 <div className="filter-type-container md-text">
-                    <input type="radio" id="one-rating" className="input-radio" />
-                    <label for="one-rating">1 stars & above</label>
+                    <input type="radio" id="one-rating" className="input-radio" name="rating-sort" value={"1 stars & above<"} checked={state.sortByCategory === "1 stars & above"} onChange={(e)=>dispatch({type: "FILTER_RATING" , payload: e.target.value})}/>
+                    <label htmlFor="one-rating">1 stars & above</label>
                 </div>
 
 
             </div>
+
+                       {/* Sort By Price Filter */}
+
             <div className="filter-type">
                 <h3 className="filter-type-heading lg-text">Sort by</h3>
                 <div className="filter-type-container md-text">
-                    <input type="radio" id="low-high" className="input-radio" />
-                    <label for="low-high">Price-Low to High</label>
+                    <input type="radio" id="low-high" className="input-radio" name="sort" value="Low-to-High" 
+          onChange={() => dispatch({ type: "SORT", payload: "LOW_TO_HIGH" })}/>
+                    <label htmlFor="low-high">Price-Low to High</label>
                 </div>
                 <div className="filter-type-container md-text">
-                    <input type="radio" id="high-low" className="input-radio" />
-                    <label for="high-low">Price-High to Low</label>
+                    <input type="radio" id="high-low" className="input-radio"  name="sort" value="High-to-Low" 
+          onChange={() => dispatch({ type: "SORT", payload: "HIGH_TO_LOW" })}  />
+                    <label htmlFor="high-low">Price-High to Low</label>
                 </div>
             </div>
         </aside>
