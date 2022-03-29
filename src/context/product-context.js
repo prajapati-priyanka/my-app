8a8209c91392fe96862d394054a04c877eaf5ad0
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-03-29 13:05:10 +0530

    feat: Add product-context
    created a product context for product page.

---
+++ src/context/product-context.js
@@ -0,0 +1,53 @@
+import { createContext,useContext, useEffect,useReducer  } from "react";
+import axios from "axios";
+import { sortReducer } from "../reducer/product-filter-reducer";
+import { sortPrice,filteredProduct,filterPrice,filterCategory,filterRating} from "../utilities/utilis";
+
+
+
+const ProductContext = createContext();
+
+const initialState={
+    isLoading: false,
+    dataList: [],
+    sortBy: null,
+    setError: null,
+    showAll: true,
+    priceValue: 0,
+    sortByCategory: null,
+    sortByRating: null
+    
+
+}
+
+const ProductProvider = ({children}) => {
+  const [state, dispatch] = useReducer(sortReducer, initialState);
+
+  useEffect(()=>{
+      (async()=>{
+          try {
+              const res = await axios.get("./api/products")
+              console.log("data from api", res.data.products)
+              dispatch({type:"LOAD_DATA", payload: res.data.products})
+          } catch (error) {
+              dispatch({type: "ERROR", payload:"Error Occurred"})
+              
+          }
+      })();
+  },[])
+
+const sortedData = sortPrice(state.dataList, state.sortBy);
+const filteredData = filteredProduct(sortedData,state);
+const filteredPriceData = filterPrice(filteredData, state.priceValue);
+const filteredCategoryData = filterCategory(filteredPriceData, state.sortByCategory)
+const finalfilteredList = filterRating(filteredCategoryData, state.sortByRating)
+// console.log("in context", sortedData)
+
+    return <ProductContext.Provider value={{state, finalfilteredList, dispatch}}>
+        {children}
+    </ProductContext.Provider>
+}
+
+const useProduct = () => useContext(ProductContext);
+
+export{ProductProvider, useProduct }
