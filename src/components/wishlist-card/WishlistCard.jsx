bc22bbdbddd1fa64ed715a6cbde58a9f317b1525
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-03-31 04:05:16 +0530

    feat: Add close icon and import wishlist-context

--- src/components/wishlist-card/WishlistCard.jsx
+++ src/components/wishlist-card/WishlistCard.jsx
@@ -1,19 +1,30 @@
-import { MdFavorite } from "react-icons/md";
+import { MdClose } from "react-icons/md";
+import { BsStarFill } from "react-icons/bs";
 import "./WishlistCard.css"
+import { useWishList } from "../../context";
 
-const WishlistCard = (props) =>{
- const {title, subtitle, image,price} = props
+const WishlistCard = ({products}) =>{
+   const {wishListDispatch} = useWishList();
+  const {title, subtitle , price,image , rating} = products
     return (
-        <div className="card vertical-card card-shadow">
+        <div className="card vertical-card card-with-dismiss card-shadow">
                 <figure className="card-header">
                     <img src={image} alt={subtitle} />
                 </figure>
-                <button className="card-floating-icon">
-                    <MdFavorite title = "Added to Wishlist"/>
-                   
-                </button>
+               
+                <button class="close-icon">
+                       <MdClose className="lg-text" title="Remove from Wishlist" onClick={()=>wishListDispatch({type: "REMOVE_PRODUCT_FROM_WISHLIST", payload: products})}/>
+                 </button>
+                
                 <section className="card-body">
                     <h5 className="card-title md-text">{title}</h5>
+                    <div className={rating >3 ? "rating-badge bg-green" : "rating-badge bg-yellow"}>
+                <div className="rating-container">
+                    <span className="rating-number">{rating}</span>
+                      <BsStarFill className="rating-star"/>
+
+                </div>
+            </div>
                     <p className="card-subtitle md-text">{subtitle}</p>
                     <div className="card-price">
                         <span className="price-after-discount md-text">{price}</span>
