e9d3aa1d60061759d34342ee4b0c5889c4293db3
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-04-02 15:45:50 +0530

    feat: Added add and delete functionality
    addProductToWishlist, deleteProductFromWishlist functionality added.

--- src/context/wishList-context.js
+++ src/context/wishList-context.js
@@ -1,26 +1,84 @@
-import { createContext, useContext, useReducer  } from "react";
+import { createContext, useContext, useReducer, useEffect } from "react";
+import axios from "axios";
 import { wishListReducer } from "../reducer/wishList-reducer";
 
-
 const wishListInitialState = {
-    wishListItem : [],
-    wishListCount: null
-}
+  wishListItem: [],
+  wishListCount: null,
+};
 
+const encodedToken =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiIxZGU4ZGU2OC04YTljLTQzN2UtYjQ1OS0zYmExMWE4ZmVlYzgiLCJlbWFpbCI6ImFkYXJzaGJhbGlrYUBnbWFpbC5jb20ifQ.xr5Ma2P_rdAfNdlRySy8wgmsrEtfwBXaM_5m2w5xZ84";
 
-
+const config = {
+  headers: {
+    authorization: encodedToken,
+  },
+};
 const WishListContext = createContext();
 
-const WishListProvider = ({children}) => {
-    
-    const [wishListState, wishListDispatch] = useReducer(wishListReducer, wishListInitialState)
-   
-   return <WishListContext.Provider value={{wishListState, wishListDispatch}}>
-        {children}
+const WishListProvider = ({ children }) => {
+  const [wishListState, wishListDispatch] = useReducer(
+    wishListReducer,
+    wishListInitialState
+  );
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await axios.get("/api/user/wishlist", config);
+        wishListDispatch({
+          type: "LOAD_DATA",
+          payload: response.data.wishlist,
+        });
+      } catch (err) {
+        console.log(err);
+      }
+    })();
+  }, []);
+
+  const addProductToWishList = async (products) => {
+    try {
+      const response = await axios.post(
+        "/api/user/wishlist",
+        { product: products },
+        config
+      );
+
+      wishListDispatch({
+        type: "ADD_PRODUCT_TO_WISHLIST",
+        payload: response.data.wishlist,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const deleteProductFromWishlist = async (products) => {
+    try {
+      const response = await axios.delete(
+        `/api/user/wishlist/${products._id}`,
+        config
+      );
+
+      wishListDispatch({
+        type: "DELETE_PRODUCT_FROM_WISHLIST",
+        payload: response.data.wishlist,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <WishListContext.Provider
+      value={{ wishListState, addProductToWishList, deleteProductFromWishlist }}
+    >
+      {children}
     </WishListContext.Provider>
-}
+  );
+};
 
-const useWishList = () => useContext(WishListContext)
+const useWishList = () => useContext(WishListContext);
 
-export {useWishList, WishListProvider}
-
+export { useWishList, WishListProvider };
