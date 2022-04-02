45dcc22e9b86ed752369c0eba21a1171463491e6
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-04-02 15:53:59 +0530

    feat: Add link to product page

--- src/pages/wishlist/Wishlist.jsx
+++ src/pages/wishlist/Wishlist.jsx
@@ -1,23 +1,34 @@
-import {Nav, WishlistCard,Footer} from "../../components";
+import { Nav, WishlistCard, Footer } from "../../components";
 import { useWishList } from "../../context";
-import "./Wishlist.css"
+import { Link } from "react-router-dom";
+import "./Wishlist.css";
 
 const Wishlist = () => {
-    const {wishListState} = useWishList();
+  const { wishListState } = useWishList();
 
-    const{wishListItem} = wishListState;
-    return (
-        <div className="wishlist-page">
-            <Nav />
-            <main>
-            <h3 className="page-title text-center lg-text">My Wishlist({wishListItem.length})</h3>
-            <div className ="wishlist-container">
-                {wishListItem.length===0 ? <h2 className="no-product-text">No items wishlisted</h2> : 
-                wishListItem.map(product => <WishlistCard products ={product} key={product._id} /> )}
-            </div>
-            </main>
-            <Footer />
+  const { wishListItem} = wishListState;
+  
+  return (
+    <div className="wishlist-page">
+      <Nav />
+      <main>
+        <h3 className="page-title text-center lg-text">
+          My Wishlist({wishListItem.length})
+        </h3>
+        <div className="wishlist-container">
+          {wishListItem.length === 0 ? (
+            <h2 className="no-product-text">
+              No items wishlisted..<Link to="/product">Let's Shop</Link>
+            </h2>
+          ) : (
+            wishListItem.map((product) => (
+              <WishlistCard products={product} key={product._id} />
+            ))
+          )}
         </div>
-    )
-}
-export {Wishlist}
+      </main>
+      <Footer />
+    </div>
+  );
+};
+export { Wishlist };
