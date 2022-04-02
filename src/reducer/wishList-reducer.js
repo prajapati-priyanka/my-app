051f040eb790bc4ff077b8a6897f4fbac573afc1
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-04-02 15:52:53 +0530

    feat: Add all cases for wishlist

--- src/reducer/wishList-reducer.js
+++ src/reducer/wishList-reducer.js
@@ -1,26 +1,28 @@
-const wishListReducer = (state, action) =>{
+const wishListReducer = (state, action) => {
 
-    console.log("In wishlist reducer",state, action)
-    switch (action.type) {
-        case "ADD_PRODUCT_TO_WISHLIST":
-            return{
-                ...state,
-                wishListItem: [...state.wishListItem, action.payload],
-                wishListCount: state.wishListCount+1
-            }
-        case "REMOVE_PRODUCT_FROM_WISHLIST":
-            return{
-                ...state,
-                wishListItem: state.wishListItem.filter(item=> item._id !== action.payload._id),
-                wishListCount: state.wishListCount-1
-            }
-            
-    
-        default:
-           return  state
-    }
+  switch (action.type) {
+    case "LOAD_DATA":
+      return {
+        ...state,
+        wishListItem: [...action.payload],
+      };
 
+    case "ADD_PRODUCT_TO_WISHLIST":
+      return {
+        ...state,
+        wishListItem: [...action.payload],
+        wishListCount: state.wishListCount + 1,
+      };
+    case "DELETE_PRODUCT_FROM_WISHLIST":
+      return {
+        ...state,
+        wishListItem: [...action.payload],
+        wishListCount: state.wishListCount - 1,
+      };
 
-}
+    default:
+      return state;
+  }
+};
 
-export{wishListReducer}
+export { wishListReducer };
