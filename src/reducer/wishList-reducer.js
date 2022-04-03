5f909e19e091de29fd9af4f946162ba2c617a9b7
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-04-03 15:54:34 +0530

    fix: Remove unwanted wishListCount state

--- src/reducer/wishList-reducer.js
+++ src/reducer/wishList-reducer.js
@@ -5,19 +5,20 @@ const wishListReducer = (state, action) => {
       return {
         ...state,
         wishListItem: [...action.payload],
+       
       };
 
     case "ADD_PRODUCT_TO_WISHLIST":
       return {
         ...state,
         wishListItem: [...action.payload],
-        wishListCount: state.wishListCount + 1,
+       
+       
       };
     case "DELETE_PRODUCT_FROM_WISHLIST":
       return {
         ...state,
         wishListItem: [...action.payload],
-        wishListCount: state.wishListCount - 1,
       };
 
     default:
