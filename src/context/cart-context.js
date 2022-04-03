f6be8d75bfb994c6244fead868b658aee68a9ebc
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-04-03 16:46:24 +0530

    feat: import cartReducer

--- src/context/cart-context.js
+++ src/context/cart-context.js
@@ -1,11 +1,17 @@
-import { createContext, useContext } from "react/cjs/react.production.min";
+import { createContext, useContext, useReducer } from "react";
+import { cartReducer } from "../reducer/cart-reducer";
 
+const initialState = {
+    cartItem: []
+}
 
 const CartContext = createContext();
 
 const useCart = () => useContext(CartContext);
 
 const CartProvider = ({children}) => {
+
+    const [ cartState, cartDispatch ] = useReducer(cartReducer, initialState)
     return <CartContext.Provider>
         {children}
     </CartContext.Provider>
