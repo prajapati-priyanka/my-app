919841f1b5090abc623cf2629adba7ec71a1536c
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-04-03 20:28:27 +0530

    feat: Add cases for cart

--- src/reducer/cart-reducer.js
+++ src/reducer/cart-reducer.js
@@ -1,6 +1,24 @@
 const cartReducer = (state, action) =>{
-    return{
-
+    
+    switch (action.type) {
+        case "LOAD_DATA":
+            return{
+                cartItem: [...action.payload]
+            }
+            
+        case "ADD_TO_CART":
+            return{
+                cartItem: [...action.payload]
+            }
+            
+        case "REMOVE_FROM_CART":
+            return{
+                cartItem: [...action.payload]
+            }
+            
+    
+        default:
+            return state
     }
 }
 
