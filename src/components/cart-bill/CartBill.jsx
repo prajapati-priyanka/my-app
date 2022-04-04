5e2dc6d9f22b1c0640030e66733e05f21eb6a476
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-04-05 01:44:31 +0530

    style: remove media query

--- src/components/cart-bill/CartBill.jsx
+++ src/components/cart-bill/CartBill.jsx
@@ -1,50 +1,58 @@
-import "./CartBill.css"
-const CartBill = ({products}) =>{
+import "./CartBill.css";
+const CartBill = ({ products }) => {
+  const { cartItem } = products;
+  console.log("incartBill", cartItem);
 
-    const {cartItem} = products
-    console.log("incartBill",cartItem)
+  const totalBagPrice = cartItem.reduce(
+    (acc, curr) => (acc = curr.qty * curr.priceBeforeDiscount + acc),
+    0
+  );
+  const totalDiscountPrice = cartItem.reduce(
+    (acc, curr) =>
+      (acc = curr.qty * curr.priceBeforeDiscount * (curr.discount / 100) + acc),
+    0
+  );
+  const finalPrice = totalBagPrice - totalDiscountPrice;
+  const deliveryCharge = 100;
 
-    const totalBagPrice = cartItem.reduce((acc,curr) => acc = (curr.qty * curr.priceBeforeDiscount) + acc ,0);
-    const totalDiscountPrice = cartItem.reduce((acc,curr)=> acc = (curr.qty*curr.priceBeforeDiscount*(curr.discount/100))+ acc ,0)
-    const finalPrice = totalBagPrice - totalDiscountPrice;
-   const deliveryCharge = 100;
+  const finalCheckoutPrice = Number(finalPrice) + Number(deliveryCharge);
 
-   const finalCheckoutPrice = Number(finalPrice) + Number(deliveryCharge);
-    
-   
-    
-
-    return(
-        <div className="bill-container">
-        <div className="bill-card">
-            <h5 className="card-title lg-text">PRICE DETAILS</h5>
-            <div className="items-container">
-                <div className="items-price md-text">
-                    <p className="item-type">Price ({cartItem.length} {cartItem.length===1 ? "item" : "items"})</p>
-                    <p className="item-type-price">₹{totalBagPrice}</p>
-                </div>
-                <div className="items-price md-text">
-                    <p className="item-type">Discount</p>
-                    <p className="item-type-price">- ₹{totalDiscountPrice}</p>
-                </div>
-                <div className="items-price md-text">
-                    <p className="item-type">Delivery</p>
-                    <p className="item-type-price">₹{deliveryCharge}</p>
-                </div>
-            </div>
-            <div className="items-price total-price-container">
-                <p className="item-type total-price lg-text">Total Amount</p>
-                <p className="item-type-price total-price-value  md-text">₹{finalCheckoutPrice}</p>
-            </div>
-            <div className="items-price total-saving-container">
-                <p className="item-type total-saving md-text">You will save ₹ {totalDiscountPrice} on this order </p>
-            
-            </div>
-            
-            <button className="btn btn-primary">PLACE ORDER</button>
+  return (
+    <div className="bill-container">
+      <div className="bill-card">
+        <h5 className="card-title lg-text">PRICE DETAILS</h5>
+        <div className="items-container">
+          <div className="items-price md-text">
+            <p className="item-type">
+              Price ({cartItem.length}{" "}
+              {cartItem.length === 1 ? "item" : "items"})
+            </p>
+            <p className="item-type-price">₹{totalBagPrice}</p>
+          </div>
+          <div className="items-price md-text">
+            <p className="item-type">Discount</p>
+            <p className="item-type-price">- ₹{totalDiscountPrice}</p>
+          </div>
+          <div className="items-price md-text">
+            <p className="item-type">Delivery</p>
+            <p className="item-type-price">₹{deliveryCharge}</p>
+          </div>
+        </div>
+        <div className="items-price total-price-container">
+          <p className="item-type total-price lg-text">Total Amount</p>
+          <p className="item-type-price total-price-value  md-text">
+            ₹{finalCheckoutPrice}
+          </p>
+        </div>
+        <div className="items-price total-saving-container">
+          <p className="item-type total-saving md-text">
+            You will save ₹ {totalDiscountPrice} on this order{" "}
+          </p>
         </div>
 
-     </div>
-    )
-}
-export {CartBill}
+        <button className="btn btn-primary">PLACE ORDER</button>
+      </div>
+    </div>
+  );
+};
+export { CartBill };
