f3c76ab2850af30ebf3e0c4ff956a0ed900b3641
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-04-04 21:27:45 +0530

    feat: Add incrementQuantity function

--- src/context/cart-context.js
+++ src/context/cart-context.js
@@ -26,7 +26,7 @@ useEffect(() => {
     (async () => {
       try {
         const response = await axios.get("/api/user/cart", config);
-        console.log("in cart response", response.data.cart)
+        // console.log("in cart response", response.data.cart)
         cartDispatch({
           type: "LOAD_DATA",
           payload: response.data.cart,
@@ -37,15 +37,46 @@ useEffect(() => {
     })();
   }, []);
 
+ const itemExistInCart = (products) =>{
+   let flag = false;
+   cartState.cartItem.map(item => {
+     if (item._id === products._id){
+       flag=true;
+     }
+   });
+return flag;
+ }
+
   const addToCart = async(products,setIsDisabled) =>{
         try{
-            setIsDisabled(true)
+          const  itemInCart = itemExistInCart(products);
+          
+          if (itemInCart) {
+
+           const data = {
+              action: {
+                type: "increment"
+              },
+            };
+
+            const response = await axios.post(`/api/user/cart/${products._id}`, data, config);
+            console.log("already in cart..", response)
+            cartDispatch({
+              type: "ADD_TO_CART",
+              payload: response.data.cart ,
+            });
+          }
+          else{
+            console.log("in else part", products)
+            setIsDisabled(true);
             const response = await axios.post("/api/user/cart", {product: products}, config);
             if(response.status === 201){
                 cartDispatch({type:"ADD_TO_CART", payload: response.data.cart})
             }else{
                 throw new Error("Can't process the request")
             }
+          }
+            
         }catch(error){
             alert(error)
         }finally{
@@ -64,7 +95,32 @@ useEffect(() => {
         alert(error)
     }
 }
-    return <CartContext.Provider value={{cartState, cartDispatch, addToCart,removeFromCart}}>
+
+const incrementQuantity = async (products) => {
+  console.log("increment quantity",products);
+  try {
+
+    const response = await axios.post(
+      `/api/user/cart/${products._id}`,
+      {
+        action: { type: "increment" },
+      },
+      config
+    );
+    if (response.status === 200) {
+      cartDispatch({
+        type: "UPDATE",
+        payload:  response.data.cart ,
+      });
+    }else{
+      throw new Error("Can't process the request")
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+    return <CartContext.Provider value={{cartState, cartDispatch, addToCart,removeFromCart,incrementQuantity}}>
         {children}
     </CartContext.Provider>
 }
