330dfc60017e620164a1181b2f26a556230113dd
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-04-15 00:36:34 +0530

    feat: Add image

--- src/backend/db/categories.js
+++ src/backend/db/categories.js
@@ -9,31 +9,31 @@ export const categories = [
   {
     _id: uuid(),
     categoryName: "Top",
-    description:
-      "literature in the form of prose, especially novels, that describes imaginary events and people",
+    image:"/assets/categories/top.jpg"
+   
   },
   {
     _id: uuid(),
     categoryName: "Jeans",
-    description:
-      "Non-fiction is writing that gives information or describes real events, rather than telling a story.",
+    image:"/assets/categories/jeans.jpg"
+    
   },
   {
     _id: uuid(),
     categoryName: "Dresses",
-    description:
-      "Meant to cause discomfort and fear for both the character and readers, horror writers often make use of supernatural and paranormal elements in morbid stories that are sometimes a little too realistic.",
+    image:"/assets/categories/dresses.jpg"
+   
   },
   {
     _id: uuid(),
     categoryName: "Kurta Set",
-    description:
-      "Meant to cause discomfort and fear for both the character and readers, horror writers often make use of supernatural and paranormal elements in morbid stories that are sometimes a little too realistic.",
+    image:"/assets/categories/kurta.jpg"
+    
   },
   {
     _id: uuid(),
     categoryName: "Sarees",
-    description:
-      "Meant to cause discomfort and fear for both the character and readers, horror writers often make use of supernatural and paranormal elements in morbid stories that are sometimes a little too realistic.",
+    image:"/assets/categories/saree.jpg"
+   
   },
 ];
