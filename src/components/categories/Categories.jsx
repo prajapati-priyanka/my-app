0b71c6bc8f7d22a09dc357d7ca3424b885403a71
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-04-15 03:07:11 +0530

    feat: Use useState hook to show the categories

--- src/components/categories/Categories.jsx
+++ src/components/categories/Categories.jsx
@@ -1,17 +1,38 @@
 import "./Categories.css";
-import { useCategory } from "../../context/categories-context";
 import { CategoryCard } from "../category-card/CategoryCard";
+import { useState,useEffect } from "react";
+import axios from "axios";
 
 const Categories = () => {
-    const {categoryState} = useCategory();
+    const [categories, setCategories] = useState([]);
+    useEffect(()=>{
+        (async()=>{
+            try{
 
-    const {categoryItem} = categoryState;
+                const response = await axios.get("/api/categories")
+                
+                if(response.status === 200){
+
+                    setCategories([...response.data.categories])
+                }else{
+                    throw new Error("Couldn't process the request ")
+                }
+
+            }catch(err){
+                console.error(err)
+            }
+        })()
+
+    },[])
+    
+
+
     return (
         <section className="product-category">
             <h2 className="category-heading text-center">Categories To Bag</h2>
             <div className="category-display">
-             { categoryItem.map(product => (
-                 <CategoryCard products = {product} key={product.id} />
+             { categories.map(product => (
+                 <CategoryCard products = {product} key={product._id} />
              ))  }
 
             </div>
