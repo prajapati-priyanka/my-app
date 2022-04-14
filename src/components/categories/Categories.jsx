64122af1dc01d1c6b61218e753d95f4990cbd9fc
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-04-15 00:35:51 +0530

    feat: Import CategoryCard component

--- src/components/categories/Categories.jsx
+++ src/components/categories/Categories.jsx
@@ -1,52 +1,20 @@
 import "./Categories.css";
+import { useCategory } from "../../context/categories-context";
+import { CategoryCard } from "../category-card/CategoryCard";
 
 const Categories = () => {
+    const {categoryState} = useCategory();
+
+    const {categoryItem} = categoryState;
     return (
         <section className="product-category">
             <h2 className="category-heading text-center">Categories To Bag</h2>
             <div className="category-display">
-                <div className="card ecommerce-card">
-                    <div className="card-with-text-overlay">
-                        <figure className="card-header">
-                            <img src="/assets/categories/top.jpg" alt="" />
-                        </figure>
-                    </div>
-                <div className="text-overlay lg-text">Top</div>
-            </div>
-            <div className="card ecommerce-card">
-                <div className="card-with-text-overlay">
-                    <figure className="card-header">
-                        <img src="/assets/categories/jeans.jpg" className="img-responsive" alt="" />
-                    </figure>
-                </div>
-                <div className="text-overlay lg-text">Jeans</div>
-            </div>
-            <div className="card ecommerce-card">
-                <div className="card-with-text-overlay">
-                    <figure className="card-header">
-                        <img src="/assets/categories/dresses.jpg" className="img-responsive" alt="" />
-                    </figure>
-                </div>
-                <div className="text-overlay lg-text">Dresses</div>
-            </div>
-            <div className="card ecommerce-card">
-                <div className="card-with-text-overlay">
-                    <figure className="card-header">
-                        <img src="/assets/categories/saree.jpg" className="img-responsive" alt="" />
-                    </figure>
-                </div>
-                <div className="text-overlay lg-text">Saree</div>
-            </div>
-            <div className="card ecommerce-card">
-                <div className="card-with-text-overlay">
-                    <figure className="card-header">
-                        <img src="/assets/categories/kurta.jpg" className="img-responsive" alt="" />
-                    </figure>
-                </div>
-                <div className="text-overlay lg-text">Kurta Set</div>
-            </div>
+             { categoryItem.map(product => (
+                 <CategoryCard products = {product} key={product.id} />
+             ))  }
 
-        </div>
+            </div>
     </section>
     )
 }
