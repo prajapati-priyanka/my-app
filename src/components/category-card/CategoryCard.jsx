47a26642c1771821cbc417b288f3507752cd98b2
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-04-15 03:07:51 +0530

    feat: Add onClick functionality to category-card

--- src/components/category-card/CategoryCard.jsx
+++ src/components/category-card/CategoryCard.jsx
@@ -1,20 +1,47 @@
-const CategoryCard = ({products}) => {
+import axios from "axios";
+import { useProduct } from "../../context/product-context";
+import { useNavigate } from "react-router-dom";
 
-    const {categoryName, image} = products;
-    console.log("in CategoryCard", products)
-    return (
-        <>
-        <div className="card ecommerce-card">
-                    <div className="card-with-text-overlay">
-                        <figure className="card-header">
-                            <img src={image} alt="" />
-                        </figure>
-                    </div>
-                <div className="text-overlay lg-text">{categoryName}</div>
-            </div>
-            
-        </>
-    )
-}
+const CategoryCard = ({ products }) => {
+  const navigate = useNavigate();
+  const { filterDispatch } = useProduct();
 
-export {CategoryCard};
+  const getCategory = async (products) => {
+    try {
+      const response = await axios.get(`/api/categories/${products._id}`);
+      if (response.status === 200) {
+        filterDispatch({
+          type: "SORT_BY_CATEGORY",
+          payload: response.data.category.categoryName,
+        });
+      } else {
+        throw new Error("Can't process the request");
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const { categoryName, image } = products;
+
+  return (
+    <>
+      <div
+        className="card ecommerce-card"
+        onClick={() => {
+          navigate("/product");
+          getCategory(products);
+        }}
+      >
+        <div className="card-with-text-overlay">
+          <figure className="card-header">
+            <img src={image} alt="" />
+          </figure>
+        </div>
+        <div className="text-overlay lg-text">{categoryName}</div>
+      </div>
+    </>
+  );
+};
+
+export { CategoryCard };
