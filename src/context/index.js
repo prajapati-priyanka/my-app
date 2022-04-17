9e8c52f125cb2b04e41df39ae53a87db94150fa0
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-04-17 19:10:58 +0530

    feat: Import AuthProvider and useAuth

--- src/context/index.js
+++ src/context/index.js
@@ -1,3 +1,4 @@
-export {ProductProvider, useProduct} from "./product-context";
-export{WishListProvider, useWishList} from "./wishList-context";
-export {CartProvider, useCart} from "./cart-context"
+export { ProductProvider, useProduct } from "./product-context";
+export { WishListProvider, useWishList } from "./wishList-context";
+export { CartProvider, useCart } from "./cart-context";
+export { AuthProvider, useAuth } from "./auth-context";
