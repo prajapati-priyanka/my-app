17b03c88b37edca1d933fc273c15609412eab32d
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-04-17 19:14:54 +0530

    refactor: remove commented code

--- src/context/auth-context.js
+++ src/context/auth-context.js
@@ -1,23 +1,11 @@
 import { useContext, createContext, useReducer } from "react";
 import { authReducer } from "../reducer/auth-reducer";
 
-// const token = localStorage.getItem("token");
-// // const userName = JSON.parse(localStorage.getItem("user.firstName"));
-// // const userEmail = JSON.parse(localStorage.getItem("user.email"));
-// // const userPassword = JSON.parse(localStorage.getItem("user_password"));
-
-// const initialState = {
-//   isLoggedIn: token ? true : false,
-//   token: token,
-// //   userName: userName,
-// //   email: userEmail,
-// //   password: userPassword,
-// };
-
 const initialState = {
   user: "",
   token: "",
 };
+
 const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
