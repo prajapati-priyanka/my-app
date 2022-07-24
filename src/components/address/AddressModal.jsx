fcfe0075d7b60a7af98907d1cfaf75a922f0c2db
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-07-24 21:53:45 +0530

    feat: Add new address from address modal

--- src/components/address/AddressModal.jsx
+++ src/components/address/AddressModal.jsx
@@ -1,75 +1,134 @@
+import { useState } from "react";
 import "./AddressModal.css";
+import {useAuth} from "../../context";
+import {addNewAddressHandler} from "../../utilities";
+import { useNavigate } from "react-router-dom";
 
-const AddressModal = ({showAddressModal}) => {
+const AddressModal = ({ showAddressModal, setShowAddressModal }) => {
+  const [address, setAddress] = useState({
+    name: "",
+    street: "",
+    city: "",
+    state: "",
+    country: "",
+    zipCode: "",
+    mobile: "",
+  });
+
+  const {
+    authState: { token },
+    authDispatch,
+  } = useAuth();
+
+  const navigate = useNavigate()
+
+  const dummyAddress = {
+    name: "Guest User",
+    street: "Flat No. 1D, MA Road, Rajendranagar",
+    city: "Indore",
+    state: "Madhya Pradesh",
+    country: "India",
+    zipCode: "781003",
+    mobile: "8839579485",
+  };
+
+  const changeHandler = (event) => {
+    const { name, value } = event.target;
+    setAddress({ ...address, [name]: value });
+};
+
+const callAddNewAddressHandler = () => {
+  if (token) {
+      addNewAddressHandler(address, authDispatch, token);
+      setShowAddressModal(false);
+  }
+  else {
+      navigate("/login");
+      // toast.warning("You're not logged in");
+  }
+}
+
   return (
     <>
-    { showAddressModal ?
-    (<div className="modal-background">
-      <div className="modal-container address-modal">
-        <div className="address-form">
-          <h3 className="form-heading text-center">Add New Address</h3>
+      {showAddressModal ? (
+        <div className="modal-background">
+          <div className="modal-container address-modal">
+            <div className="address-form">
+              <h3 className="form-heading text-center">Add New Address</h3>
 
-          <form>
-            <input 
-              type="text"
-              placeholder="Enter your name.."
-              name="name"
-              required
-            />
-            <input
-              type="text"
-              placeholder="Enter house no, street or colony"
-              name="content"
-              required
-            />
-            <input type="text" placeholder="Enter City" name="city" required />
-            <input
-              type="text"
-              placeholder="Enter State"
-              name="state"
-              required
-            />
-            <input
-              type="text"
-              placeholder="Enter Country"
-              name="country"
-              required
-            />
-            <input
-              type="number"
-              placeholder="Enter Zip Code"
-              name="zipCode"
-              required
-            />
-            <input
-              type="number"
-              placeholder="Enter Mobile Number"
-              name="mobile"
-              required
-            />
-            <div className="form-action-btns">
-              <button
-                className="btn btn-solid-primary action-btn save-btn"
-               >
-                Save
-              </button>
-              <button
-                className="btn outline-btn-primary action-btn dummy-btn"
-
-              >
-                Dummy Address
-              </button>
-              <button
-                className="btn outline-btn-primary action-btn cancel-btn"
-              >
-                Cancel
-              </button>
+              <form>
+                <input
+                  type="text"
+                  placeholder="Enter your name.."
+                  name="name"
+                  value={address.name}
+                  onChange={changeHandler}
+                  required
+                />
+                <input
+                  type="text"
+                  placeholder="Enter house no, street or colony"
+                  name="content"
+                  value={address.street}
+                  onChange={changeHandler}
+                  required
+                />
+                <input
+                  type="text"
+                  placeholder="Enter City"
+                  name="city"
+                  value={address.city}
+                  onChange={changeHandler}
+                  required
+                />
+                <input
+                  type="text"
+                  placeholder="Enter State"
+                  name="state"
+                  value={address.state}
+                  onChange={changeHandler}
+                  required
+                />
+                <input
+                  type="text"
+                  placeholder="Enter Country"
+                  name="country"
+                  value={address.country}
+                  onChange={changeHandler}
+                  required
+                />
+                <input
+                  type="number"
+                  placeholder="Enter Zip Code"
+                  name="zipCode"
+                  value={address.zipCode}
+                  onChange={changeHandler}
+                  required
+                />
+                <input
+                  type="number"
+                  placeholder="Enter Mobile Number"
+                  name="mobile"
+                  value={address.mobile}
+                  onChange={changeHandler}
+                  required
+                />
+                <div className="form-action-btns">
+                  <button className="btn-solid-primary action-btn save-btn" onClick={callAddNewAddressHandler}>
+                    Save
+                  </button>
+                  <button className="outline-btn-primary action-btn dummy-btn" onClick={()=> setAddress(dummyAddress)}>
+                    Dummy Address
+                  </button>
+                  <button className="outline-btn-primary action-btn cancel-btn" onClick={()=>setShowAddressModal(false)}>
+                    Cancel
+                  </button>
+                </div>
+              </form>
             </div>
-          </form>
+          </div>
         </div>
-      </div>
-    </div>)
-    : null}
+      ) : null}
     </>
   );
 };
