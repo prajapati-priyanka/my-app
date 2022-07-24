90f12bb711168a74313c01cf6ef91855708ddff3
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-07-24 21:53:11 +0530

    feat: get address in address component

--- src/components/address/Address.jsx
+++ src/components/address/Address.jsx
@@ -1,21 +1,57 @@
 import "./Address.css";
-import { GrAdd} from "react-icons/gr";
+import { GrAdd } from "react-icons/gr";
+import { useAuth } from "../../context";
 
+const Address = ({ setShowAddressModal }) => {
+  const {
+    authState: { token, addresses },
+    authDispatch,
+  } = useAuth();
 
-const Address = ({setShowAddressModal})=>{
+  const updateAddress = (address) => {
+    setEditAddress(address);
+    setShowAddressModal(true);
+  };
 
-    const addNewAddress = ()=>{
-        console.log("clicked")
-        setShowAddressModal(true)
-    }
-    return(
-   <div className="address-container">
-       <button className="btn new-address-btn md-text" onClick={addNewAddress}>
+  useEffect(() => getAddressesHandler(token, authDispatch), []);
+
+  const addNewAddress = () => {
+    setShowAddressModal(true);
+  };
+  return (
+    <>
+    {addresses ? (  <div className="address-container">
+      {addresses.map((item) => (
+        <div className="address" key={item._id}>
+          <h3>{item.name}</h3>
+          <p>{item.street}</p>
+          <p>
+            {item.city},{item.state},{item.zipCode}{" "}
+          </p>
+          <p>{item.country}</p>
+          <p>Mobile: {item.mobile}</p>
+          <div className="address-action-btns">
+            <button
+              className="btn btn-solid-primary btn-edit"
+              onClick={() => setShowAddressModal(true)}
+            >
+              Edit
+            </button>
+            <button className="btn btn-outline-primary btn-remove">
+              Remove
+            </button>
+          </div>
+        </div>
+      ))}
+
+      <button className="btn new-address-btn md-text" onClick={addNewAddress}>
         <GrAdd />
         Add New Address
-       </button>
-   </div>
-    )
-}
+      </button>
+    </div>): null}
+  
+    </>
+  );
+};
 
-export {Address}
+export { Address };
--- src/context/auth-context.js
+++ src/context/auth-context.js
@@ -1,9 +1,10 @@
-import { useContext, createContext, useReducer, useEffect } from "react";
+import { useContext, createContext, useReducer } from "react";
 import { authReducer } from "../reducer/auth-reducer";
 
 const initialState = {
   user: "",
   token: "",
+  addresses: []
 };
 
 const AuthContext = createContext();
@@ -11,13 +12,15 @@ const AuthContext = createContext();
 const AuthProvider = ({ children }) => {
   const [authState, authDispatch] = useReducer(authReducer, initialState);
 
-  const checkUser = () => {
+ 
+
+  if(!authState.token){
     const token = localStorage.getItem("token");
     const user = JSON.parse(localStorage.getItem("user"));
-    authDispatch({ type: "CHECK_USER", payload: { user, token } });
-  };
+    authDispatch({type: "CHECK_USER", payload:{user,token}})
+  }
 
-  useEffect(() => checkUser(), []);
+
 
   return (
     <AuthContext.Provider value={{ authState, authDispatch }}>
