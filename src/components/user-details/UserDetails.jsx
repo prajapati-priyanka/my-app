2ae964f42ee993f43a3fd95cbc662ad68f6c960e
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-07-25 14:10:32 +0530

    feat: Add logged in user details

--- src/components/user-details/UserDetails.jsx
+++ src/components/user-details/UserDetails.jsx
@@ -1,18 +1,24 @@
+import { useAuth } from "../../context"
 import "./UserDetails.css"
 
 
 const UserDetails = () =>{
+
+    const {authState} = useAuth();
+
+   const user = authState.user || JSON.parse(localStorage.getItem("user"))
+  
     return(
         <div className="user-page">
             <div className="user-details">
                 <h3 className=" detail lg-text">Profile Details</h3>
                 <div className="user-info md-text">
                     <h4>Name:</h4>
-                    <p>Adarsh Balika</p>
+                    <p>{user.firstName} {user.lastName}</p>
                 </div>
                 <div className="user-info md-text">
                     <h4>Email:</h4>
-                    <p>adarshBalika@gmail.com</p>
+                    <p>{user.email}</p>
                 </div>
             </div>
         </div>
