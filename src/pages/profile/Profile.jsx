6ef6f58c6209e37feb0a6d74e11dc153ce98b443
prajapati-priyanka <contact.priyankaprajapati@gmail.com>
2022-07-25 14:47:11 +0530

    style:  active tabs

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,41 +1,66 @@
 import { useState } from "react";
-import { Nav, Footer, AddressModal, UserDetails, Address, Logout } from "../../components";
+import {
+  Nav,
+  Footer,
+  AddressModal,
+  UserDetails,
+  Address,
+  Logout,
+} from "../../components";
 import "./Profile.css";
+import {useLocation, useNavigate, Outlet} from "react-router-dom"
 
-
-
-const Profile = () =>{
-  const [activeTab, setActiveTab] = useState("profile")
+const Profile = () => {
   const [showAddressModal, setShowAddressModal] = useState(false);
   const [editAddress, setEditAddress] = useState(null);
+  const location = useLocation();
+  const navigate = useNavigate();
 
-    return(
-        <>
-        {showAddressModal ? <AddressModal showAddressModal={showAddressModal} setShowAddressModal={setShowAddressModal} editAddress={editAddress} setEditAddress={setEditAddress} /> : null}
-        <Nav />
-        <main>
-          <h3 className="page-title text-center lg-text">
-           Account
-          </h3>
-          <section className="profile-page">
-            <div className="profile-tabs">
-                <button className="tab lg-text" onClick={()=>setActiveTab("profile")}>
-                    Profile
-                </button>
-                <button className="tab lg-text" onClick={()=>setActiveTab("address")}>
-                    My Address
-                </button>
-                <button className="tab lg-text" onClick={()=>setActiveTab("logout")} >
-                    Settings
-                </button>
+  return (
+    <>
+      {showAddressModal ? (
+        <AddressModal
+          showAddressModal={showAddressModal}
+          setShowAddressModal={setShowAddressModal}
+          editAddress={editAddress}
+          setEditAddress={setEditAddress}
+        />
+      ) : null}
+      <Nav />
+      <main>
+        <h3 className="page-title text-center lg-text">Account</h3>
+        <section className="profile-page">
+          <div className="profile-tabs">
+            <button
+              className={`tab ${location.pathname=== "/profile" ? "tab-active" : ""} lg-text`}
+              onClick={() => navigate("/profile")}
+            >
+              Profile
+            </button>
+            <button
+              className={`tab ${
+                location.pathname === "/profile/address" ? "tab-active" : ""
+              } lg-text`}
+              onClick={() => navigate("address")}
+            >
+              My Address
+            </button>
+            <button
+              className={`tab order-tab ${
+                location.pathname === "/profile/logout" ? "tab-active" : ""
+              } lg-text`}
+              onClick={() => navigate("logout")}
+            >
+              Settings
+            </button>
+          </div>
+          {location.pathname === "/profile" ? <UserDetails /> : null}
+          <Outlet />
+        </section>
+      </main>
+      <Footer />
+    </>
+  );
+};
 
-            </div>
-  {activeTab === "profile" ? <UserDetails /> : activeTab === "address" ? <Address  setShowAddressModal={setShowAddressModal} setEditAddress={setEditAddress}/> : <Logout />}
-          </section>
-        </main>
-        <Footer />
-      </>
-    )
-}
-
-export {Profile}
+export { Profile };
